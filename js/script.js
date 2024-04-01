1fac41ad800fd8030f3d7bf5498da4f29e6cbd6b
Lucas Waunn <lwaunn@hawk.iit.edu>
2024-04-01 17:20:49 -0500

    fixing formatting to match final

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 function init(){
   var button = document.getElementById('entrybutton');
+
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
     alert("Daniel Krieglstein: " + textbox.value);
   }
+  
   button.addEventListener('click', showMeText);
   }
 
