a7180c128eb07ff950c614f3875ebf7db51d304c
Lucas Waunn <lwaunn@hawk.iit.edu>
2024-04-01 17:25:07 -0500

    Changed name after finding I didn't need exact for comparison image

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function init(){
   function showMeText(){
     var textbox = document.getElementById('entryinput');
     document.getElementById('textoutput').innerHTML = textbox.value;
-    alert("Daniel Krieglstein: " + textbox.value);
+    alert("Lucas Waunn: " + textbox.value);
   }
   
   button.addEventListener('click', showMeText);
